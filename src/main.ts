eaacc5c47273133b067d42d090e47f7466dea1b0
Suwijak Pitsupan (King) <suwijak1691@gmail.com>
2026-04-08 10:47:26 +0700

    Update Vercel configuration to include Swagger UI files and simplify Swagger setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,14 +24,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customCssUrl:
-      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css',
-    customJs: [
-      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js',
-      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-standalone-preset.js',
-    ],
-  });
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
